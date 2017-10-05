b9e61da8f9826f199e9a0e293496a76939cc4a35
Michael Hashe <mhashe@caltech.edu>
2017-10-04 20:10:44 -0700

    Mysh.c not fully connected with token.c

--- src/shell/mysh.c
+++ src/shell/mysh.c
@@ -5,41 +5,59 @@
 
 #include "token.h"
 
-#define MAX_SIZE	1024	// Max size of input
+#define MAX_SIZE    1024    // Max size of input
 
 int main(int argc, char *argv[])
 {
-	// User ID. Function guaranteed to be successful.
-	uid_t uid = getuid();
+    // User ID. Function guaranteed to be successful.
+    uid_t uid = getuid();
 
-	// Struct containing user information
-	struct passwd *pw = getpwuid(uid);
-	if (!pw)
-	{
-		perror("Error in getpwuid");
-		return(1);
-	}
-	// User name
-	char *uname = pw->pw_name;
+    // Struct containing user information
+    struct passwd *pw = getpwuid(uid);
+    if (!pw)
+    {
+        perror("Error in getpwuid");
+        return(1);
+    }
+    // User name
+    char *uname = pw->pw_name;
 
-	// Path length at most PATH_MAX
-	char cwd[PATH_MAX];
-	if (getcwd(cwd, sizeof(cwd)) == NULL) {
-		perror("Error in getcwd");
-		return(1);
-	}
+    // Path length at most PATH_MAX
+    char cwd[PATH_MAX];
+    if (getcwd(cwd, sizeof(cwd)) == NULL) {
+        perror("Error in getcwd");
+        return(1);
+    }
 
-	while(1) {
-		char commands[MAX_SIZE];
+    while(1) {
+        char command[MAX_SIZE];
 
-		// Print command prompt
-		printf("%s:%s >>> ", uname, cwd);
+        // Print command prompt
+        printf("%s:%s >>> ", uname, cwd);
 
-		// Wait for input
-		fgets(commands, MAX_SIZE, stdin);
+        // TODO: Handle SIGINT
+        
+        // Wait for input
+        if (fgets(command, MAX_SIZE, stdin) == NULL) {
+            perror("Error in fgets");
+            return(1);
+        }
 
+        // Tokenize input
+        char** comms = tokenize(command);
 
-	}
+        char* comm;
+        int i = 0;
+        while(1) {
+            comm = comms[i];
+            if (comm == NULL) {
+                break;
+            }
+            printf("%s\n", comm);
+            i++;
 
-	return(0);
+        }
+    }
+
+    return(0);
 }
