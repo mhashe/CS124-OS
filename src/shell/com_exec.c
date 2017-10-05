00100bd9a17fe52df5a2b3c18ae53e240b6568ee
Vaibhav Anand <vanand@caltech.edu>
2017-10-04 22:54:31 -0700

    Removed binary

--- src/shell/com_exec.c
+++ src/shell/com_exec.c
@@ -19,6 +19,52 @@
 
 // TODO: How to figure out when the children processes have finished execution? Get this info
 
+/* Takes in a linked list of commands and executes them while redirecting their 
+io. It then waits for all of them to finish in the shell process. */
+int fork_commands(struct command *cmd) {
+    int last_out_fd = -1; // there was no previous fd to take as input
+
+    while (1) {
+        pid_t pid;
+        int fd[2];
+
+        if (cmd->next != NULL) {
+            if (pipe(fd) < 0)
+                return -1; // pipe error
+        }
+        if ((pid = fork()) < 0) {
+            return -1; // fork error
+        }
+        if (pid > 0) {
+            // we are in the shell process
+            if (last_out_fd != -1) {
+                close(last_out_fd);
+            } 
+            if (cmd->next == NULL) {
+                close(fd[1]);
+                return 0; // we are done forking all commands
+            } else {
+                last_out_fd = fd[1];
+                cmd = cmd->next; // process next command in next iter
+            }
+        }
+        else {
+            // we are in the child process
+            // if there was a previous command, set its out to this one's input
+            if (last_out_fd != -1) {
+                dup2(last_out_fd, STDIN_FILENO);
+                close(last_out_fd);
+            }
+            // if there is a next command, set this one's out to be pipe-input
+            if (cmd->next != NULL) {
+                dup2(fd[1], STDOUT_FILENO);
+                close(fd[1]);
+            }
+            execute_command(cmd);
+        }
+    }
+    return 0;
+}
 
 /* Executes the command after forking and does the io redirection to stdout, 
 stdin, or stderr if needed. The io redirection is done after forking, so the 
@@ -40,7 +86,6 @@ void execute_command(struct command *cmd) {
         dup2(fd2, STDERR_FILENO);
         close(fd2);
     }
-    fflush(NULL);
 
     // Execute the command with its arguments
     execvp(cmd->exec_fn, cmd->argv);
@@ -52,26 +97,11 @@ void execute_command(struct command *cmd) {
 
 
 int main(int argc, char *argv[]) {
-    // make sure everything is null terminated?
-    char *args[3];
-    char arg = '/';
-    args[0] = "echo";
-    args[1] = "hi";
-    args[2] = NULL;
-    // pid_t pid = fork();
-    // int status;
-    execvp("echo", args);
-    return 0;
-
     printf("test:\n");
-    struct command cmd;
-    cmd.exec_fn = "/bin/ls";
-    cmd.argv = args;
-    cmd.input_fn = NULL;
-    cmd.output_fn = "";
-    cmd.error_fn = "";
-    cmd.next = NULL;
-    execute_command(&cmd);
+
+    // test chaining commands:
+
+
 
     return 0;
 }
@@ -79,6 +109,21 @@ int main(int argc, char *argv[]) {
 
 
 /* Deprecated testing functions
+
+void test_single_command() {
+    char *args[3];
+    args[0] = "wc";
+    args[1] = NULL;
+    args[2] = NULL;
+    struct command cmd;
+    cmd.exec_fn = "wc";
+    cmd.argv = args;
+    cmd.input_fn = "notes.out";
+    cmd.output_fn = "count.out";
+    cmd.error_fn = "";
+    cmd.next = NULL;
+    execute_command(&cmd);
+}
 
 void test_piping() {
     int n; // number of lines read/written from file
