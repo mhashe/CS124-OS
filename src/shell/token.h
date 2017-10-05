b867f1f5e25525f5a78512feb5b54b7aeab7d740
Nikhil Gupta <nikhil97gupta@gmail.com>
2017-10-05 03:17:28 -0700

    can rerun old command using !n

--- src/shell/token.h
+++ src/shell/token.h
@@ -2,6 +2,7 @@
 #define TOKEN_H
 
 void copy_comm(char** dest, char* src, int word_start, int word_size, int i);
+int is_number(char* str, int start_i, int end_i);
 char** tokenize(char* commands);
 
 #endif
