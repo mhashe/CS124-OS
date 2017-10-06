beefeae8426e8ba22c6fb2d4b80d874c7b45a2ee
Nikhil Gupta <nikhil97gupta@gmail.com>
2017-10-05 21:48:50 -0700

    add file header to token.c

--- src/shell/consts.h
+++ src/shell/consts.h
@@ -5,8 +5,10 @@
 #ifndef CONSTS_H
 #define CONSTS_H
 
-
+// max number of bytes accepted for a single command 
 #define MAX_LINE 1024
+
+// file descriptors
 #define STDIN_FILENO 0
 #define STDOUT_FILENO 1
 #define STDERR_FILENO 2
