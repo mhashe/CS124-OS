91c98c90ebbefea43dca3415e133527372737305
Michael Hashe <mhashe@caltech.edu>
2017-10-13 15:14:54 -0700

    Game Structs moved to .h file
    Makes both files a bit more readable.

--- src/booter/game.h
+++ src/booter/game.h
@@ -50,6 +50,54 @@ invading enemies. */
 #define GO_LOST 1
 #define GO_WON 2
 
+/* A struct used for holding x-y pairs
+ * for bullets.
+ */
+typedef struct pair {
+    int x;
+    int y;
+} Pair;
+
+/* Struct holding global game
+ * state, parameters.
+ */
+typedef struct Space_Invaders {
+    /* Info presented to user */
+    uint8_t info_bar_height;
+    uint8_t game_over;
+
+    /* Position and direction of enemies */
+    // top, left corner off enemy matrix. all are pixel units
+    uint16_t enemy_mat_position_x; 
+    uint16_t enemy_mat_position_y;
+    uint16_t enemy_mat_width;
+    uint16_t enemy_mat_height;
+    int8_t enemy_direction;
+
+    uint16_t num_enemies_left;
+
+    /* array of ints containing num enemies per column (assume no 
+    gaps between enemies in a column, so it represents a matrix) */
+    uint8_t num_enemy_cols;
+    uint8_t num_enemy_rows;
+    // matrix of enemies (1 if alive/visible, 0 if dead/invisible)
+    uint8_t enemy_mat[NUM_ENEMY_COLS][(VID_HEIGHT / (ALIEN_SIZE + ENEMY_SPACING))];
+
+    // user position
+    uint8_t user_bar_height;
+    uint8_t user_position_x;
+    uint8_t user_position_y; // always the lowest row (not dynamic)
+
+    // bullet queue, counter
+    Pair bullet_queue[MAX_BULLETS];
+    int bullet_counter;
+
+    // alien bullet queue, counter
+    Pair en_bullet_queue[ENEMY_BULLETS];
+    int en_bullet_counter;
+
+} Space_Invaders;
+
 void c_start(void);
 
 void init_game_state(void);
