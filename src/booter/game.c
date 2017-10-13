0c3c4f7c0a7e7bcddf3c561d1964775dbf1b4a3c
Vaibhav Anand <vanand@caltech.edu>
2017-10-12 19:41:09 -0700

    Added game initialization
    Initialization creates the game structure using defined constants in game.h

--- src/booter/game.c
+++ src/booter/game.c
@@ -2,6 +2,9 @@
 #include "timer.h"
 #include "keyboard.h"
 #include "video.h"
+#include "game.h"
+
+#include <stdint.h>
 
 /* This is the entry-point for the game! */
 void c_start(void) {
@@ -24,29 +27,66 @@ void c_start(void) {
 }
 
 
-// we have 80x25 canvas
 typedef struct Space_Invaders {
-    // info presented to user
+    /* Info presented to user */
+    uint8_t info_bar_height;
     uint32_t score; // number of enemies killed
     uint8_t lives_remaining;
 
-    // position and direction of enemies
-    // top, left corner off enemy matrix
-    uint8_t enemy_position_x;
-    uint8_t enemy_position_y;
-    uint16_t num_enemies_left; // (16bit in case resolution were to increase)
+    /* Position and direction of enemies */
+    // top, left corner off enemy matrix. all are pixel units
+    uint16_t enemy_mat_position_x; 
+    uint16_t enemy_mat_position_y;
+    uint16_t enemy_mat_width;
+    uint16_t enemy_mat_height;
+
+    uint16_t num_enemies_left;
+
+    /* array of ints contraining num enemies per column (assume no 
+    gaps between enemies in a column, so it represents a matrix) */
+    uint8_t num_enemy_cols;
+    uint8_t num_enemies_per_col[NUM_ENEMY_COLS];
 
     // user position
+    uint8_t user_bar_height;
     uint8_t user_position_x;
     uint8_t user_position_y; // always the lowest row (not dynamic)
 
-    // matrix of booleans stating whether enemy
-    uint16_t offset_15_0;      // offset bits 0..15
-    uint16_t selector;         // a code segment selector in GDT or LDT
-    uint8_t zero;              // unused, set to 0
-    uint8_t type_attr;         // descriptor type and attributes
-    uint16_t offset_31_16;     // offset bits 16..31
 } Space_Invaders;
 
 static Space_Invaders game_state;
 
+
+void initialize_game() {
+    game_state.score = 0;
+    game_state.lives_remaining = NUM_LIVES;
+
+    game_state.info_bar_height = INFO_BAR_HEIGHT * VID_HEIGHT;
+    game_state.user_bar_height = USER_BAR_HEIGHT * VID_HEIGHT;
+    game_state.user_position_y = VID_HEIGHT - game_state.user_bar_height;
+    // user starts off in middle of screen
+    game_state.user_position_x = (VID_WIDTH - USER_SIZE) / 2;
+
+    // enemies start of in aligned center, right below the info space
+    // height enemy mat will use
+    game_state.enemy_mat_height = ((VID_HEIGHT - game_state.info_bar_height 
+        - game_state.user_bar_height) * ENEMY_MAT_HEIGHT);
+    game_state.enemy_mat_width = VID_WIDTH * ENEMY_MAT_WIDTH;
+    // enemy starts off at center of screen
+    game_state.enemy_mat_position_x = ((VID_WIDTH - 
+        game_state.enemy_mat_width) / 2);
+    game_state.enemy_mat_position_y = game_state.user_bar_height; // start at top
+
+    // set maximum # enemies per col to each col's num_enemies_per_col
+    uint8_t max_enemies_per_col = (game_state.enemy_mat_height / 
+        (ENEMY_SIZE + ENEMY_SPACING));
+    game_state.num_enemy_cols = ((VID_WIDTH * ENEMY_MAT_WIDTH) / 
+        (ENEMY_SIZE + ENEMY_SPACING));
+
+    for (int c = 0; c < game_state.num_enemy_cols; c++) {
+        game_state.num_enemies_per_col[c] = max_enemies_per_col;
+    }
+
+    game_state.num_enemies_left = (max_enemies_per_col * 
+        game_state.num_enemy_cols);
+}
--- src/booter/game.h
+++ src/booter/game.h
@@ -1,7 +1,11 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include "video.h"
+
 /* Define constants/preferences for the game */
+
+#define NUM_LIVES 3
 
 /* Game update frequency */
 #define GAME_FPS 60 // user and missile movement udpates at this rate
@@ -9,8 +13,23 @@
 
 /* Floats in [0, 1] of fraction of available canvas's dedicated to for space 
 invading enemies*/
-#define GAME_ENEMY_WIDTH 0.5
-#define GAME_ENEMY_HEIGHT 0.5
+#define ENEMY_MAT_WIDTH 0.5
+#define ENEMY_MAT_HEIGHT 0.5
+#define ENEMY_MAT_WIDTH_PX 160
+#define NUM_ENEMY_COLS ENEMY_MAT_WIDTH_PX / (ENEMY_SIZE + ENEMY_SPACING)
+
+/* Number of pixels in between space invaders on right and left */
+#define ENEMY_SIZE 5 // enemies are drawn in squares of this size
+#define ENEMY_SPACING 5 // pixels between enemies
+
+/* Floats in [0, 1] */
+#define INFO_BAR_HEIGHT 0.1
+#define USER_BAR_HEIGHT 0.1
+#define USER_SIZE 20 // pixels of width
+
 
 
 #endif /* GAME_H */
+
+
+
