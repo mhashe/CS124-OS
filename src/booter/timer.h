52779b8e395bfafc5d1de652c7cbab2f60e872e2
Nikhil Gupta <nikhil97gupta@gmail.com>
2017-10-13 01:47:31 -0700

    Add tone generation.
    The speaker is now tied to timer 2 and generates can generate a tone.

--- src/booter/timer.h
+++ src/booter/timer.h
@@ -23,5 +23,18 @@ uint32_t get_time(void);
 void sleep(float sec);
 
 
+/*=============================================================================
+ * Generates a tone of frequency freq.
+ *
+ * Inputs:
+ *     freq: frequency to be sounded in Hz. 
+ */
+void sound(uint32_t freq);
+
+/*=============================================================================
+ * Turn off sound.
+ */
+void no_sound()
+
 #endif /* TIMER_H */
 
