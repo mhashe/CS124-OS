5210a30fb7fcf032282266781f0977f28ad92795
Nikhil Gupta <nikhil97gupta@gmail.com>
2017-10-13 15:38:45 -0700

    Numbers added a sprite constants
    Added constant bitmaps for numbers to sprites.h giving the
    functionality to print out numbers.

--- src/booter/sprites.h
+++ src/booter/sprites.h
@@ -13,6 +13,9 @@
 #define BULLET_WIDTH  3
 #define BULLET_HEIGHT 5
 
+#define FONT_WIDTH  3
+#define FONT_HEIGHT 5
+
 static const uint8_t alien[][ALIEN_SIZE] = {
     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
@@ -63,6 +66,86 @@ static const uint8_t alien_bullet[BULLET_HEIGHT][BULLET_WIDTH] = {
     {1, 1, 1},
     {0, 1, 0}, 
     {0, 1, 0}
+};
+
+static const uint8_t one[FONT_HEIGHT][FONT_WIDTH] = {
+    {0, 1, 0}, 
+    {1, 1, 0}, 
+    {0, 1, 0},
+    {0, 1, 0}, 
+    {1, 1, 1}
+};
+
+static const uint8_t two[FONT_HEIGHT][FONT_WIDTH] = {
+    {1, 1, 0}, 
+    {0, 0, 1}, 
+    {0, 1, 0},
+    {1, 0, 0}, 
+    {1, 1, 1}
+};
+
+static const uint8_t three[FONT_HEIGHT][FONT_WIDTH] = {
+    {1, 1, 0}, 
+    {0, 0, 1}, 
+    {0, 1, 0},
+    {0, 0, 1}, 
+    {1, 1, 0}
+};
+
+static const uint8_t four[FONT_HEIGHT][FONT_WIDTH] = {
+    {1, 0, 1}, 
+    {1, 0, 1}, 
+    {1, 1, 1},
+    {0, 0, 1}, 
+    {0, 0, 1}
+};
+
+static const uint8_t five[FONT_HEIGHT][FONT_WIDTH] = {
+    {1, 1, 1}, 
+    {1, 0, 0}, 
+    {1, 1, 1},
+    {0, 0, 1}, 
+    {1, 1, 1}
+};
+
+static const uint8_t six[FONT_HEIGHT][FONT_WIDTH] = {
+    {1, 1, 1}, 
+    {1, 0, 0}, 
+    {1, 1, 1},
+    {1, 0, 1}, 
+    {1, 1, 1}
+};
+
+static const uint8_t seven[FONT_HEIGHT][FONT_WIDTH] = {
+    {1, 1, 1}, 
+    {0, 0, 1}, 
+    {0, 1, 1},
+    {1, 1, 0}, 
+    {1, 0, 0}
+};
+
+static const uint8_t eight[FONT_HEIGHT][FONT_WIDTH] = {
+    {1, 1, 1}, 
+    {1, 0, 1}, 
+    {1, 1, 1},
+    {1, 0, 1}, 
+    {1, 1, 1}
+};
+
+static const uint8_t nine[FONT_HEIGHT][FONT_WIDTH] = {
+    {1, 1, 1}, 
+    {1, 0, 1}, 
+    {1, 1, 1},
+    {0, 0, 1}, 
+    {0, 0, 1}
+};
+
+static const uint8_t zero[FONT_HEIGHT][FONT_WIDTH] = {
+    {1, 1, 1}, 
+    {1, 0, 1}, 
+    {1, 0, 1},
+    {1, 0, 1}, 
+    {1, 1, 1}
 };
 
 #endif /* SPRITES_H */
