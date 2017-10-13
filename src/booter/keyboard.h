81a4357fbae24369e6ece8b77fc5644d2e8b5116
Nikhil Gupta <nikhil97gupta@gmail.com>
2017-10-12 23:52:40 -0700

    Clean up keyboard comments

--- src/booter/keyboard.h
+++ src/booter/keyboard.h
@@ -1,10 +1,25 @@
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 
-#define KEY_QUEUE_EMPTY 0xFB // an unused key code
+#define KEYBOARD_PORT 0x60
+#define KEYBOARD_CMD_PORT 0x64      // use for LED functions
 
+#define KEYCODE_QUEUE_SIZE 4
+
+#define KEY_QUEUE_EMPTY 0xFB        // an unused key code
+
+/*=============================================================================
+ * Sets up keyboard queue indexes and bind irq1_handler to timer interrupts.
+ */
 void init_keyboard(void);
 
+/*=============================================================================
+ * Returns the oldest key press scan code from the queue. Turns off interrupts
+ * to prevent an interrupt from messing up queue state. 
+ *
+ * Output:
+ *     keycode: key press scan code
+ */
 char key_queue_pop(void);
 
 #endif /* KEYBOARD_H */
