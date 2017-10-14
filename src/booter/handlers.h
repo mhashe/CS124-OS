50be8f35d8462d8ae174c09f0096d060278efb05
Michael Hashe <mhashe@caltech.edu>
2017-10-13 19:44:56 -0700

    Added include guards to handlers.h

--- src/booter/handlers.h
+++ src/booter/handlers.h
@@ -1,3 +1,7 @@
+#ifndef GAME_H
+#define GAME_H
+
+
 /* Declare the entry-points to the interrupt handler assembly-code fragments,
  * so that the C compiler will be happy.
  */
@@ -5,4 +9,7 @@
 
 void *(irq0_handler)(void); /* Timer Handler. */
 void *(irq1_handler)(void); /* Keyboard Handler. */
+
+
+#endif /* GAME_H */
 
