2cc9cb8f74d428ecdd276167772be5b583e0eef5
Michael Hashe <mhashe@caltech.edu>
2017-10-27 20:58:28 -0700

    Commented fixedp.c/h
    Added comments to fixedp files.

--- src/lib/kernel/fixedp.c
+++ src/lib/kernel/fixedp.c
@@ -1,6 +1,7 @@
 #include "fixedp.h"
 #include "../debug.h"
 
+
 #include <stdio.h>
 #include <stdint.h>
 
@@ -9,11 +10,14 @@ fixedp fixedp_from_int(int n) {
     return n * fp_f;
 }
 
+
 int fixedp_to_int_floored(fixedp x) {
     return x / fp_f;
 }
 
+
 int fixedp_to_int_nearest(fixedp x) {
+    /* Perturb number so that divison has desired effect. */
     if (x >= 0) {
         return (x + fp_f / 2) / fp_f;
     } else {
@@ -21,33 +25,41 @@ int fixedp_to_int_nearest(fixedp x) {
     }
 }
 
+
 fixedp fixedp_add(fixedp x, fixedp y) {
     return x + y;
 }
+
 
 fixedp fixedp_subtract(fixedp x, fixedp y) {
     return x - y;
 }
 
+
 fixedp fixedp_add_with_int(fixedp x, int n) {
     return x + n * fp_f;
 }
+
 
 fixedp fixedp_sub_from_fp(fixedp x, int n) {
     return x - n * fp_f;
 }
 
+
 fixedp fixedp_multiply(fixedp x, fixedp y) {
     return ((int64_t) x) * y / fp_f;
 }
+
 
 fixedp fixedp_multiple_with_int(fixedp x, int n) {
     return x * n;
 }
 
+
 fixedp fixedp_divide(fixedp x, fixedp y) {
     return ((int64_t) x) * y / fp_f;
 }
+
 
 fixedp fixedp_divide_by_int (fixedp x, int n) {
     return x / n;
--- src/lib/kernel/fixedp.h
+++ src/lib/kernel/fixedp.h
@@ -1,37 +1,57 @@
 #ifndef __LIB_FIXED_P_H
 #define __LIB_FIXED_P_H
 
-/* 
- * Comment what fixedp is. why is used. how it is used. 
- */
 
 #define fp_f    (2<<(14-1)) /* Using 17.14 fixed point representation. */
 
-/* A fixed point / kernel float. */
+
+/* A fixed point number, i.e. a number scaled by fp_f. */
 typedef int fixedp;
 
+
+/* Converts int into fixed-point by scaling up. */
 fixedp fixedp_from_int(int n);
 
+
+/* Converts int into fixed-point by scaling down, rounding towards zero. */
 int fixedp_to_int_floored(fixedp x);
 
+
+/* Converts int into fixed-point by scaling down, towards nearest int. */
 int fixedp_to_int_nearest(fixedp x);
 
+
+/* Adds two fixed-pont numbers, returning a new fixedp. */
 fixedp fixedp_add(fixedp x,  fixedp y);
 
+
+/* Subtracts two fixed-point numbers, returning a new fixedp. */
 fixedp fixedp_subtract(fixedp x,  fixedp y);
 
+
+/* Adds an integer to a fixed-point number, returning a new fixedp. */
 fixedp fixedp_add_with_int( fixedp x,  int n);
 
+
+/* Subtracts an integer from a fixedp, returning a new fixedp. */
 fixedp fixedp_sub_from_fp(fixedp x, int n);
 
+
+/* Multiplies two fixedp numbers, returning a new fixedp. */
 fixedp fixedp_multiply(fixedp x, fixedp y);
 
+
+/* Multiplies a fixedp with an int, returning a new fixedp. */
 fixedp fixedp_multiple_with_int(fixedp x, int n);
 
+
+/* Divides two fixedp numbers, returning a new fixedp. */
 fixedp fixedp_divide(fixedp x, fixedp y);
 
+
+/* Divides a fixedp by an int, returning a new fixedp. */
 fixedp fixedp_divide_by_int (fixedp x, int n);
 
 
+#endif /* lib/kernel/fixedp.h */
 
-#endif /* lib/kernel/kfloat.h */
