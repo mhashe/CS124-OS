716a14547df990c0a846522b4268f533fb1ee4ad
Vaibhav Anand <vanand@caltech.edu>
2017-10-27 18:38:27 -0700

    Fix function definitions in fixedp.h

--- src/lib/kernel/fixedp.h
+++ src/lib/kernel/fixedp.h
@@ -2,34 +2,34 @@
 #define __LIB_FIXED_P_H
 
 /* 
- * Comment what kfloat is. why is used. how it is used. 
+ * Comment what fixedp is. why is used. how it is used. 
  */
 
 
 /* A fixed point / kernel float. */
 typedef int fixedp;
 
-fixedp fixedp_from_int(int n UNUSED);
+fixedp fixedp_from_int(int n);
 
-int fixedp_to_int_rounded(fixedp x UNUSED);
+int fixedp_to_int_rounded(fixedp x);
 
-int fixedp_to_int_nearest(fixedp x UNUSED);
+int fixedp_to_int_nearest(fixedp x);
 
-fixedp fixedp_add(UNUSED fixedp x, UNUSED fixedp y);
+fixedp fixedp_add(fixedp x,  fixedp y);
 
-fixedp fixedp_subtract(UNUSED fixedp x, UNUSED fixedp y);
+fixedp fixedp_subtract(fixedp x,  fixedp y);
 
-fixedp fixedp_add_with_int(UNUSED fixedp x, UNUSED int n);
+fixedp fixedp_add_with_int( fixedp x,  int n);
 
-fixedp fixedp_sub_from_fp(UNUSED fixedp x, UNUSED int n);
+fixedp fixedp_sub_from_fp(fixedp x, int n);
 
-fixedp fixedp_multiply(UNUSED fixedp x, UNUSED fixedp y);
+fixedp fixedp_multiply(fixedp x, fixedp y);
 
-fixedp fixedp_multiple_with_int(UNUSED fixedp x, UNUSED int n);
+fixedp fixedp_multiple_with_int(fixedp x, int n);
 
-fixedp fixedp_divide(UNUSED fixedp x, UNUSED fixedp y);
+fixedp fixedp_divide(fixedp x, fixedp y);
 
-fixedp fixedp_divide_by_intUNUSED (fixedp x, UNUSED int n);
+fixedp fixedp_divide_by_int (fixedp x, int n);
 
 
 
