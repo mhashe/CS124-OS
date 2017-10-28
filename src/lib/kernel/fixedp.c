07ffbb43962f12b6471c4095717de1b0d656022b
Michael Hashe <mhashe@caltech.edu>
2017-10-27 20:51:46 -0700

    Filled in fixed point arithmetic
    Shell functions filled in

--- src/lib/kernel/fixedp.c
+++ src/lib/kernel/fixedp.c
@@ -1,47 +1,55 @@
-#include <fixedp.h>
+#include "fixedp.h"
 #include "../debug.h"
+
+#include <stdio.h>
+#include <stdint.h>
 
 
 fixedp fixedp_from_int(int n) {
-    // TODO
+    return n * fp_f;
 }
 
-int fixedp_to_int_rounded(fixedp x) {
-    // TODO
+int fixedp_to_int_floored(fixedp x) {
+    return x / fp_f;
 }
 
 int fixedp_to_int_nearest(fixedp x) {
-    // TODO
+    if (x >= 0) {
+        return (x + fp_f / 2) / fp_f;
+    } else {
+        return (x - fp_f / 2) / fp_f;
+    }
 }
 
 fixedp fixedp_add(fixedp x, fixedp y) {
-    // TODO
+    return x + y;
 }
 
 fixedp fixedp_subtract(fixedp x, fixedp y) {
-    // TODO
+    return x - y;
 }
 
 fixedp fixedp_add_with_int(fixedp x, int n) {
-    // TODO
+    return x + n * fp_f;
 }
 
 fixedp fixedp_sub_from_fp(fixedp x, int n) {
-    // TODO
+    return x - n * fp_f;
 }
 
 fixedp fixedp_multiply(fixedp x, fixedp y) {
-    // TODO
+    return ((int64_t) x) * y / fp_f;
 }
 
 fixedp fixedp_multiple_with_int(fixedp x, int n) {
-    // TODO
+    return x * n;
 }
 
 fixedp fixedp_divide(fixedp x, fixedp y) {
-    // TODO
+    return ((int64_t) x) * y / fp_f;
 }
 
 fixedp fixedp_divide_by_int (fixedp x, int n) {
-    // TODO
+    return x / n;
 }
+
--- src/lib/kernel/fixedp.h
+++ src/lib/kernel/fixedp.h
@@ -5,13 +5,14 @@
  * Comment what fixedp is. why is used. how it is used. 
  */
 
+#define fp_f    (2<<(14-1)) /* Using 17.14 fixed point representation. */
 
 /* A fixed point / kernel float. */
 typedef int fixedp;
 
 fixedp fixedp_from_int(int n);
 
-int fixedp_to_int_rounded(fixedp x);
+int fixedp_to_int_floored(fixedp x);
 
 int fixedp_to_int_nearest(fixedp x);
 
