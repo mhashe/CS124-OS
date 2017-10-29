a25c9b642ccb2431c45d2f7de1eec223a343dcef
Michael Hashe <mhashe@caltech.edu>
2017-10-29 01:58:21 -0700

    Yet Another Hex Character Removed
    Hex character in timer.c removed. Would do this all at once, but they don't seem to play nicely with grep.

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -151,7 +151,7 @@ void timer_ndelay(int64_t ns) {
 void timer_print_stats(void) {
     printf("Timer: %"PRId64" ticks\n", timer_ticks());
 }
-
+
 /*! Timer interrupt handler. */
 static void timer_interrupt(struct intr_frame *args UNUSED) {
     ticks++;
