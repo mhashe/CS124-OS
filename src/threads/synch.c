201870aeb211e3865d0d42972634fabeacbf26ed
Michael Hashe <mhashe@caltech.edu>
2017-10-28 21:33:29 -0700

    Changed pop_front to list_min
    Now, finds optimal next thread without having to keep lists sorted.

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -52,8 +52,6 @@ void sema_init(struct semaphore *sema, unsigned value) {
     sema->value = value;
     list_init(&sema->waiters);
 
-    ASSERT(is_sorted(list_begin(&sema->waiters), list_end(&sema->waiters), 
-        (list_less_func*) thread_queue_compare, NULL));
 }
 
 /*! Down or "P" operation on a semaphore.  Waits for SEMA's value
@@ -74,11 +72,8 @@ void sema_down(struct semaphore *sema) {
     while (sema->value == 0) {
         if (is_interior(&thread_current()->elem))
             list_remove(&thread_current()->elem);
-        thread_insert_ordered(&sema->waiters, &thread_current()->elem);
 
-        ASSERT(is_sorted(list_begin(&sema->waiters), list_end(&sema->waiters), 
-            (list_less_func*) thread_queue_compare, NULL));
-        
+        list_push_back(&sema->waiters, &thread_current()->elem);        
         thread_block();
     }
 
@@ -116,6 +111,7 @@ bool sema_try_down(struct semaphore *sema) {
 
     This function may be called from an interrupt handler. */
 void sema_up(struct semaphore *sema) {
+    struct list_elem *max, *max2;
     enum intr_level old_level;
 
     ASSERT(sema != NULL);
@@ -123,11 +119,13 @@ void sema_up(struct semaphore *sema) {
     old_level = intr_disable();
 
     if (!list_empty(&sema->waiters)) {
-        thread_unblock(list_entry(list_pop_front(&sema->waiters),
-                                  struct thread, elem));
 
-        ASSERT(is_sorted(list_begin(&sema->waiters), list_end(&sema->waiters), 
-            (list_less_func*) thread_queue_compare, NULL));
+        /* Min because this function wants (oddly enough) a < function, not > */
+        max = list_min(&sema->waiters, (list_less_func*) thread_queue_compare, NULL);
+        list_remove(max); /* Basically pop_max, but less overhead. */
+        thread_unblock(list_entry(max, struct thread, elem));
+
+
     }
     sema->value++;
     intr_set_level(old_level);
@@ -219,15 +217,11 @@ void lock_acquire(struct lock *lock) {
 
         // If couldn't get lock, that means that someone else is holding it. As
         // such, we elevate them to our priority.
-        thread_insert_ordered(&lock->semaphore.waiters, &thread_current()->elem);
+        list_push_back(&lock->semaphore.waiters, &thread_current()->elem);
         thread_current()->blocked_lock = lock;
 
         recalculate_priority(lock->holder);
         sort_ready_list();
-
-        ASSERT(is_sorted(list_begin(&lock->semaphore.waiters), 
-            list_end(&lock->semaphore.waiters), 
-            (list_less_func*) thread_queue_compare, NULL));
 
         sema_down(&lock->semaphore);
     }
