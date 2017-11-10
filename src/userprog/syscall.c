06fedf3e13901b1e8a4aee4d713ab09cd9be1ddf
Michael Hashe <mhashe@caltech.edu>
2017-11-10 01:23:28 -0800

    Aligned function definitions.
    Purely cosmetic.

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -9,19 +9,19 @@
 static void syscall_handler(struct intr_frame *);
 
 /* Handlers for Project 4. */
-static void halt(struct intr_frame *f);
-static void exit(struct intr_frame *f);
-static void exec(struct intr_frame *f);
-static void wait(struct intr_frame *f);
-static void create(struct intr_frame *f);
-static void remove(struct intr_frame *f);
-static void open(struct intr_frame *f);
+static void     halt(struct intr_frame *f);
+static void     exit(struct intr_frame *f);
+static void     exec(struct intr_frame *f);
+static void     wait(struct intr_frame *f);
+static void   create(struct intr_frame *f);
+static void   remove(struct intr_frame *f);
+static void     open(struct intr_frame *f);
 static void filesize(struct intr_frame *f);
-static void read(struct intr_frame *f);
-static void write(struct intr_frame *f);
-static void seek(struct intr_frame *f);
-static void tell(struct intr_frame *f);
-static void close(struct intr_frame *f);
+static void     read(struct intr_frame *f);
+static void    write(struct intr_frame *f);
+static void     seek(struct intr_frame *f);
+static void     tell(struct intr_frame *f);
+static void    close(struct intr_frame *f);
 
 void syscall_init(void) {
     intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
