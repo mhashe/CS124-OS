c11b29cdf331ff3fad7c0541d32666f9f8b524d4
Nikhil Gupta <nikhil97gupta@gmail.com>
2017-11-10 01:19:09 -0800

    Add ability to get syscall number
    Unsafe right now and untested. Doesn't verify any pointers.

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -10,7 +10,12 @@ void syscall_init(void) {
     intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-static void syscall_handler(struct intr_frame *f UNUSED) {
+
+static void syscall_handler(struct intr_frame *f) {
+    int *caller_stack = (int*)f->esp;
+    int syscall_number = *(caller_stack);
+
+
     printf("system call!\n");
     thread_exit();
 }
