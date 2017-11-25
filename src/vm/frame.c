23e4d4cebee86a2e478237f5189bc83bc297d0d9
Nikhil Gupta <nikhil97gupta@gmail.com>
2017-11-24 16:56:59 -0800

    Init frame table entry
    The first that a frame table entry is given to be filled,
    allocate memory for it.

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -8,12 +8,11 @@
 
 void init_frame_table(void) {
     frame_table = (struct frame_table_entry**) 
-                  malloc(sizeof(struct frame_table_entry*) * NUM_FRAME_ENTRIES);
+                calloc(sizeof(struct frame_table_entry*), NUM_FRAME_ENTRIES);
 }
 
 /* Gets the first free frame in the frame table. Returns an index. */
-int get_frame(void) {
-    // TODO: evict
+int get_empty_frame(void) {
     uint32_t i = 0;
     for (i = 0; i < NUM_FRAME_ENTRIES; i++) {
         if (frame_table[i] == NULL) {
@@ -21,8 +20,18 @@ int get_frame(void) {
         }
     }
     if (i == NUM_FRAME_ENTRIES) {
+        // TODO: evict
+        PANIC("frame table full\n");
         return -1;
     } else {
+        frame_table[i] = (struct frame_table_entry*) 
+                        calloc(sizeof(struct frame_table_entry), 1);
+
         return i;
     }
 }
+
+
+
+
+
