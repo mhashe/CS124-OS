dcbe05c15eea419602039178537712e1076f2c94
Nikhil Gupta <nikhil97gupta@gmail.com>
2017-11-24 16:47:38 -0800

    Frame table compiles now
    Fixed some compilation errors. It is still missing everything
    useful. Also added Make target

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -1,10 +1,20 @@
 #include "vm/frame.h"
+
 #include <debug.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include "threads/malloc.h"
+
+void init_frame_table(void) {
+    frame_table = (struct frame_table_entry**) 
+                  malloc(sizeof(struct frame_table_entry*) * NUM_FRAME_ENTRIES);
+}
 
 /* Gets the first free frame in the frame table. Returns an index. */
-int get_frame() {
+int get_frame(void) {
     // TODO: evict
-    int i = 0;
+    uint32_t i = 0;
     for (i = 0; i < NUM_FRAME_ENTRIES; i++) {
         if (frame_table[i] == NULL) {
             break;
--- src/vm/frame.h
+++ src/vm/frame.h
@@ -6,17 +6,21 @@
 #define VM_FRAME_H
 
 #include <debug.h>
+#include <stdint.h>
 #include "threads/vaddr.h"
 
 #define MEM_SIZE (1 << 31)
-#define NUM_FRAME_ENTRIES (MEM_SIZE / PGSIZE)
+#define NUM_FRAME_ENTRIES ((uint32_t) MEM_SIZE / PGSIZE)
 
 struct frame_table_entry {
     void* page;     /* pointer to page currently occupying entry */
 };
 
 
-struct frame_table_entry* frame_table[NUM_FRAME_ENTRIES];
+struct frame_table_entry** frame_table;
+
+void init_frame_table(void);
+int get_frame(void);
 
 
 #endif /* vm/frame.h */
