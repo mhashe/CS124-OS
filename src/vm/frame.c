70ce8a18df7327f2b6c6cc90512533a205cfe9c2
Nikhil Gupta <nikhil97gupta@gmail.com>
2017-11-24 22:34:20 -0800

    Getting a frame now palloc's
    Upon requesting a frame in the frame table, the function now also
    allocates a page of memory.

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -8,6 +8,8 @@
 
 #include "threads/malloc.h"
 #include "threads/loader.h"
+#include "threads/palloc.h"
+#include "threads/vaddr.h"
 
 void init_frame_table(void) {
     frame_table = (struct frame_table_entry**) 
@@ -21,23 +23,27 @@ void init_frame_table(void) {
 /* Gets the first free frame in the frame table. Returns an index. 
    page is the virtual memory pointer to a page that is occupying this frame. */
 int get_frame(void *page) {
-    uint32_t i = 0;
-    for (i = 0; i < init_ram_pages; i++) {
-        if (frame_table[i] == NULL) {
-            break;
-        }
-    }
+    // uint32_t i = 0;
+    // for (i = 0; i < init_ram_pages; i++) {
+    //     if (frame_table[i] == NULL) {
+    //         break;
+    //     }
+    // }
 
-    if (i == init_ram_pages) {
-        // TODO: evict
+    void *frame = palloc_get_page(PAL_ZERO | PAL_USER);
+
+
+    if (frame == NULL) {
+        // TODO: evict then palloc again
         PANIC("frame table full\n");
         return -1;
     } else {
-        frame_table[i] = (struct frame_table_entry*) 
+        int frame_number = vtop(frame);
+        frame_table[frame_number] = (struct frame_table_entry*) 
                         calloc(sizeof(struct frame_table_entry), 1);
-        frame_table[i]->page = page;
+        frame_table[frame_number]->page = page;
 
-        return i;
+        return frame_number;
     }
 }
 
