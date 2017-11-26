4aab0a3ada4d6c2e8d157f964d9710054f66107f
Vaibhav Anand <vanand@caltech.edu>
2017-11-26 01:06:54 -0800

    Add function headers for swap implementation
    Add to swap.c and swap.h

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -14,6 +14,22 @@
 // #include "filesys/file.h"     /* For file ops. */
 // #include "userprog/syscall.h"
 // #include "userprog/pagedir.h"
+#include "devices/block.h"
 
+static struct bitmap *swap_available;
 
+/* Initialize bitmap used to check which swap-slots are available. */
+void swap_init(void) {
+    // TODO
+}
+
+/* Writes data at addr into swap_slot. */
+void swap_write(int swap_slot, void *addr) {
+    // TODO
+}
+
+/* Reads data at swap_slot into addr. */
+void swap_read(int swap_slot, void *addr) {
+    // TODO
+}
 
--- src/vm/swap.h
+++ src/vm/swap.h
@@ -10,17 +10,12 @@
 // #include "userprog/syscall.h"
 // #include "threads/pte.h"
 
+void swap_init(void);
 
-struct swap_entry {
-    int fd;             /* Pointer to file to be opened. */
-    unsigned file_ofs;  /* File loaded into page at fd's offset. */
-    unsigned page_end;  /* File ends at this location in page. */
-    bool writable;      /* Whether the page is writable. */
-    bool loaded;        /* Whether data has been successfully loaded. */
-    uint32_t frame_no;  /* Frame number which data was loaded into. */
-    mapid_t mapid;      /* Map id if mapped with mmap. */
-    bool all_zero;      /* Represents an initially all-zero page. */
-};
+void swap_write(int swap_slot, void *addr);
+
+void swap_read(int swap_slot, void *addr);
+
 
 
 
