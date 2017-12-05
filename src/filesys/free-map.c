f4fde026819ec74bbc36bb43cf430fccc53c779f
Vaibhav Anand <vanand@caltech.edu>
2017-12-05 12:56:14 -0800

    Changes to free_map_allocate_single sector
    It is also used now in filesys.c instead of the original
    free_map_allocate()

--- src/filesys/free-map.c
+++ src/filesys/free-map.c
@@ -34,14 +34,16 @@ bool free_map_allocate(size_t cnt, block_sector_t *sectorp) {
     return sector != BITMAP_ERROR;
 }
 
-block_sector_t free_map_allocate_single(void) {
+bool free_map_allocate_single(block_sector_t *sectorp) {
     block_sector_t sector = bitmap_scan_and_flip(free_map, 0, 1, false);
     if (sector != BITMAP_ERROR && free_map_file != NULL &&
         !bitmap_write(free_map, free_map_file)) {
         bitmap_reset(free_map, sector);
         sector = BITMAP_ERROR;
     }
-    return sector;
+    if (sector != BITMAP_ERROR)
+        *sectorp = sector;
+    return sector != BITMAP_ERROR;
 }
 
 /*! Makes CNT sectors starting at SECTOR available for use. */
